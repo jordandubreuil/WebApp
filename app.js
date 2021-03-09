d827470578ff67b83e56579418631907b6717f15
JordanTron <jordan.dubreuil@gmail.com>
2021-03-09 13:16:08 -0500

    Ready

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
-var express = require("express")
+var express = require("express");
+var mongoose = require("mongoose");
 var app = express()
 var path = require('path');
 var port = process.env.PORT || 3000
+
+require('./db')
+
+//quick data model
+var User = mongoose.model('User', {
+    name:{
+        type:String
+    },
+    email:{
+        type:String
+    }
+})
 
 app.use(express.static(__dirname+"/public"))
 app.use(express.urlencoded({extended:true}))
@@ -11,12 +24,31 @@ app.get('/',function(req,res){
     res.sendFile(path.join(__dirname+"/public/index.html"))
 })
 
+app.get('/getData',function(req,res){
+    User.find({}).then(function(user){
+        res.json({user})
+    })
+})
+
 app.post('/data', function(req,res){
     console.log("posting data")
-    console.log(req.body, req.body.name, req.body.email)
-    res.redirect('/')
+    //console.log(req.body, req.body.name, req.body.email)
+    var newUser = new User({
+        name:req.body.name,
+        email:req.body.email
+    })
+    newUser.save(function(err,result){
+        if(err){
+            console.log(err)
+        }else{
+            console.log(result)
+            res.redirect('/userlist.html')
+        }
+    })
+ 
+    
 })
 app.listen(port,function(){
-    console.log("Listening on port 3000")
+    console.log("Listening on port " + port)
 })
 
